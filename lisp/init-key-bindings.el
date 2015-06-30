225b184a9c17d23c2e04d7b30c063222dab29911
Son Nguyen <mail@gimu.org>
2015-06-30 20:25:09 +0200

    Update config

--- lisp/init-key-bindings.el
+++ lisp/init-key-bindings.el
@@ -1,3 +1,4 @@
+
 ;; Neotree
 (global-set-key [f8] 'neotree-toggle)
 
@@ -5,6 +6,15 @@
 (global-set-key (kbd "C-<down>") 'windowmove-down)
 (global-set-key (kbd "C-<left>") 'windmove-left)
 (global-set-key (kbd "C-<right>") 'windmove-right)
+
+;; Need to switch between some modes sometimes
+(global-set-key [f1] 'text-mode)
+(global-set-key [f2] 'python-mode)
+(global-set-key [f3] 'c++-mode)
+(global-set-key [f4] 'java-mode)
+(global-set-key [f5] 'html-mode)
+(global-set-key [f6] 'php-mode)
+(global-set-key [f7] 'javascript-mode)
 
 ;; Unset mail bindings
 (global-unset-key (kbd "C-x m"))
